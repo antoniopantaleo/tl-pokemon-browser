d3628d17ceb28f335b1140bae673354e4b6ab0ae
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2025-04-24 18:54:58 +0200

    test: move StubBuilder HTTPClientStub extension in a separate file

---
+++ Tests/PokespeareTests/Helpers/StubBuilder+HTTPClientStub.swift
@@ -0,0 +1,50 @@
+//
+//  StubBuilder+HTTPClientStub.swift
+//  Pokespeare
+//
+//  Created by Antonio on 24/04/25.
+//
+
+import Foundation
+import Pokespeare
+
+struct Success {
+    
+    let statusCode: Int
+    let data: Data
+    
+    init(statusCode: Int = 200, data: () throws -> Data) {
+        self.statusCode = statusCode
+        self.data = try! data()
+    }
+}
+
+struct Failure {
+    
+    let error: Error
+    
+    init(error: Error)  {
+        self.error = error
+    }
+}
+
+extension StubBuilder where Stub == HTTPClientStub.Stub {
+    static func buildExpression(_ expression: Success) -> [Stub] {
+        [.success(
+            Response(
+                statusCode: expression.statusCode,
+                data: expression.data
+            )
+        )]
+    }
+    
+    static func buildExpression(_ expression: Failure) -> [Stub] {
+        [.failure(expression.error)]
+    }
+}
+
+extension HTTPClientStub {
+    convenience init(@StubBuilder<Stub> _ stubBuilder: () -> [Stub]) {
+        self.init(stubs: stubBuilder())
+    }
+}
--- Tests/PokespeareTests/Helpers/StubBuilder.swift
+++ Tests/PokespeareTests/Helpers/StubBuilder.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import Pokespeare
 
 @resultBuilder enum StubBuilder<Stub> {
     static func buildBlock(_ components: [Stub]...) -> [Stub] {
@@ -19,46 +18,5 @@ import Pokespeare
     
     static func buildExpression(_ expression: [Stub]) -> [Stub] {
         expression
-    }
-}
-
-struct Success {
-    
-    let statusCode: Int
-    let data: Data
-    
-    init(statusCode: Int = 200, data: () throws -> Data) {
-        self.statusCode = statusCode
-        self.data = try! data()
-    }
-}
-
-struct Failure {
-    
-    let error: Error
-    
-    init(error: Error)  {
-        self.error = error
-    }
-}
-
-extension StubBuilder where Stub == HTTPClientStub.Stub {
-    static func buildExpression(_ expression: Success) -> [Stub] {
-        [.success(
-            Response(
-                statusCode: expression.statusCode,
-                data: expression.data
-            )
-        )]
-    }
-    
-    static func buildExpression(_ expression: Failure) -> [Stub] {
-        [.failure(expression.error)]
-    }
-}
-
-extension HTTPClientStub {
-    convenience init(@StubBuilder<Stub> _ stubBuilder: () -> [Stub]) {
-        self.init(stubs: stubBuilder())
     }
 }
