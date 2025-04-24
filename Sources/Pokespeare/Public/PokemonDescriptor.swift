8080a7ac1344da98ab6f2b6a09c40422b3d3e8a7
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2025-04-24 17:19:13 +0200

    refactor(translations): split getDescription functions in separate functions

--- Sources/Pokespeare/Public/PokemonDescriptor.swift
+++ Sources/Pokespeare/Public/PokemonDescriptor.swift
@@ -22,15 +22,25 @@ public struct ShakespeareanPokemonDescriptor: PokemonDescriptor {
     }
     
     public func getDescription(pokemonName name: String) async throws -> String {
-        lazy var decoder = JSONDecoder()
+        let remotePokemonDetail = try await getPokemonDetails(pokemonName: name)
+        let englishDescription = try await getPokemonSpeciesEnglishDescription(
+            speciesURL: remotePokemonDetail.species.url
+        )
+        return try await getShakespeareanTranslation(of: englishDescription)
+    }
+    
+    private func getPokemonDetails(pokemonName name: String) async throws -> RemotePokemonDetail {
         let detailRequest = URLRequest(url: PokeAPI.pokemon(name: name).url)
         let detailResponse = try await client.perform(request: detailRequest)
         try validateResponse(detailResponse)
-        let remotePokemonDetail = try decoder.decode(RemotePokemonDetail.self, from: detailResponse.data)
-        let speciesRequest = URLRequest(url: remotePokemonDetail.species.url)
+        return try JSONDecoder().decode(RemotePokemonDetail.self, from: detailResponse.data)
+    }
+    
+    private func getPokemonSpeciesEnglishDescription(speciesURL: URL) async throws -> String  {
+        let speciesRequest = URLRequest(url: speciesURL)
         let speciesResponse = try await client.perform(request: speciesRequest)
         try validateResponse(speciesResponse)
-        let pokemonSpecies = try decoder.decode(
+        let pokemonSpecies = try JSONDecoder().decode(
             RemotePokemonSpecies.self,
             from: speciesResponse.data
         )
@@ -42,12 +52,16 @@ public struct ShakespeareanPokemonDescriptor: PokemonDescriptor {
         else {
             throw NoEnglishDescriptionError()
         }
+        return description
+    }
+    
+    private func getShakespeareanTranslation(of description: String) async throws -> String {
         let translationRequest = URLRequest(url: FunTranslationsAPI.shakespeare(
             text: description).url
         )
         let translationResponse = try await client.perform(request: translationRequest)
         try validateResponse(translationResponse)
-        let translation = try decoder.decode(
+        let translation = try JSONDecoder().decode(
             RemoteTranslationResponse.self,
             from: translationResponse.data
         )
