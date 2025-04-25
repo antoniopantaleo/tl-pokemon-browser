efdb854611e8a862524ef96a661ef053be6bb730
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2025-04-25 21:36:18 +0200

    feat: add Pokemon Browser demo app

---
+++ Pokemon Browser/Pokemon Browser/Pokemon_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_BrowserApp.swift
+//  Pokemon Browser
+//
+//  Created by Antonio on 25/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
